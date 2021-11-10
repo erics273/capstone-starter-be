d99a2be7796b333a9a2569c6fe45c1e19711a11b
James Turner <James.Turner@libertymutual.com>
2021-11-10 12:59:40 -0500

    updated controllers, routes to add additional crud functiionality

--- routes/clinics.routes.js
+++ routes/clinics.routes.js
@@ -5,12 +5,16 @@ const router = express.Router();
 const clinicsController = require("../controllers/clinics.controller")
 
 //post route to create a clinic (clinic registration)
-//router.post("/", clinicsController.createClinic)
+router.post("/", clinicsController.createClinic)
 
 //get route to return all clinics **no validator yet, not sure if needed 
 router.get("/", clinicsController.getClinics)
 
-//put route to update a user (requires auth)
-//router.put("/:email", validateJwtMiddleware, userController.updateUser)
+//put route to update a clinic **no validator yet, not sure if needed 
+router.put("/:id", clinicsController.updateClinic)
+
+//delete route to delete a clinic **no validator yet, not sure if needed 
+router.delete("/:id", clinicsController.deleteClinic)
+
 
 module.exports = router;
