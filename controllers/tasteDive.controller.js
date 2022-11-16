a153a62a0529283cffc6067b6cb172c701c283f8
ArthurBrockelman <ArthurBrockelman@gmail.com>
2022-11-16 12:02:13 -0500

    Updated node version.

--- controllers/tasteDive.controller.js
+++ controllers/tasteDive.controller.js
@@ -4,7 +4,7 @@ const tasteDiveController = {
     tasteDiveGetRelatedMedia: async function (req, res) {
         let MediaName = req.query.MediaName
         console.log(MediaName)
-        request.get(`https://tastedive.com/api/similar?q=${MediaName}&info=1&limit=100&verbose=1`, function (error, response, body) {
+        request.get(`https://tastedive.com/api/similar?q=${MediaName}&info=1&limit=10&verbose=1`, function (error, response, body) {
             res.send(response)
           }
         )
